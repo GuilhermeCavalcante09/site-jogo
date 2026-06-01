abb5425449a59d1cdf9c63761d1e5c503b2c6afb
GuilhermeCavalcante09 <guilherme.9035@colegioprogresso.g12.br>
2026-06-01 00:40:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 const games = [{
         id: 1,
         name: "Projeto - Guilherme",
-        url: "https://guilhermecavalcante09.github.io/jogo-guilherme/projeto_jogo/",
+        url: "merry-treacle-727dc3.netlify.app",
         icon: "🎮",
         desc: "Um jogo MetroidVania",
         likes: 0,
